2ae185d96bae0d466f54ca55091d026c768132de
mudabs <mudaburamunashe.com>
2025-09-16 07:31:02 -0500

    Third Commit

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve:{
-    alias:{
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  base: '/portfolio/',  // 👈 important for GitHub Pages
 })
